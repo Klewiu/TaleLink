430c26611c933116d51a86cdeec7d0f8051b472e
Jakub Klewicki <kuba.klewicki@gmail.com>
2023-11-04 22:38:28 +0100

    Refactor header & navbar. Update navbar

--- frontend/angular-frontend/src/app/layouts/header/header.component.ts
+++ frontend/angular-frontend/src/app/layouts/header/header.component.ts
@@ -1,47 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { NgbModal, NgbPaginationConfig } from '@ng-bootstrap/ng-bootstrap';
-import { ApiCallService } from 'src/app/services/api-call.service';
-import { AuthService } from 'src/app/services/auth.service';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  user_login_text = {
-    user_name: '',
-    login_text: ''
-  };
-  loggedIn = false; // Add a loggedIn property
-
-  constructor(
-    private apiService: ApiCallService,
-    private config: NgbPaginationConfig,
-    private modalService: NgbModal,
-    private authService: AuthService,
-    private router: Router
-  ) {}
-
-  ngOnInit() {
-    this.authService.isLoggedIn().subscribe((status) => {
-      this.loggedIn = status;
-
-      const userData = localStorage.getItem('currentUser');
-      if (status && userData) {
-        const user = JSON.parse(userData);
-        this.user_login_text.user_name = user.username;
-        this.user_login_text.login_text = 'USER: ';
-      } else {
-        this.user_login_text.user_name = '';
-        this.user_login_text.login_text = 'You are logged out';
-      }
-    });
-  }
-
-  logout() {
-    this.authService.logout();
-    this.router.navigate(['/login']);
-  }
+export class HeaderComponent {
 }
--- frontend/angular-frontend/src/app/layouts/navbar/navbar.component.ts
+++ frontend/angular-frontend/src/app/layouts/navbar/navbar.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal, NgbPaginationConfig } from '@ng-bootstrap/ng-bootstrap';
+import { ApiCallService } from 'src/app/services/api-call.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  user_login_text = {
+    user_name: '',
+    login_text: ''
+  };
+  loggedIn = false; // Add a loggedIn property
+
+  constructor(
+    private apiService: ApiCallService,
+    private config: NgbPaginationConfig,
+    private modalService: NgbModal,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.authService.isLoggedIn().subscribe((status) => {
+      this.loggedIn = status;
+
+      const userData = localStorage.getItem('currentUser');
+      if (status && userData) {
+        const user = JSON.parse(userData);
+        this.user_login_text.user_name = user.username;
+        this.user_login_text.login_text = ' ';
+      } else {
+        this.user_login_text.user_name = '';
+        this.user_login_text.login_text = '';
+      }
+    });
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 
 }
